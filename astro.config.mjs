6bac27160af8d9466e233abbdb29cfc3db250149
Declan <declanjcooke@gmail.com>
2025-04-14 14:42:33 +1200

    Removed @astrojs/image to fix Netlify build

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({});
 import { defineConfig } from 'astro/config';
-import image from '@astrojs/image';
+
 
 export default defineConfig({
-  integrations: [image()],
+  integrations: [],
 });
